86e09dac12f028336daa2306f4e95910163d1b68
olga fomitcheva <olga_fomitcheva@mail.ru>
2023-03-23 17:26:01 +0300

    сделан аккордион

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
 
-    $('.j-burger').on('click', function() {
-        $('.j-main-menu').fadeToggle('is-open')
-    });
+  $('.j-burger').on('click', function() {
+        $('.j-main-menu').toggleClass('is-open');
+      });
+  //Аккордионы
+  let prevAccordionBtn = undefined;
+
+  $('.j-accordion-btn').on('click', function() {
+    
+    if (prevAccordionBtn === this) {
+      $(this).next().slideToggle();
+    }else {
+      $(prevAccordionBtn).next().slideUp();
+      $(this).next().slideDown();
+      prevAccordionBtn = this;
+    }
+  })
    
 }); 
 
