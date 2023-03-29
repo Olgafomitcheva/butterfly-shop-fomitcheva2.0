9ac69e4bb0104505d62e485d0f80585f9a5a5213
olga fomitcheva <olga_fomitcheva@mail.ru>
2023-03-29 23:15:26 +0300

    сделаны фильтры и табы

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -16,7 +16,42 @@ $(document).ready(function() {
       prevAccordionBtn = this;
     }
   })
-   
-}); 
+  // Табы
+  $('.j-contacts-link').on('click', function() {
+    let index = $(this).index('.j-contacts-link');
 
+    $('.j-contacts-link').removeClass('active');
+    // $('.j-contacts-link').eq(index).addClass('active');
+    // Аналог:
+    $(this).addClass('active');
+
+    $('.j-contacts-item').removeClass('active');
+    $('.j-contacts-item').eq(index).addClass('active');
+
+  });
+
+   // Фильтр в наших работах
+   $('.j-works-link').on('click', function() {
+    $('.j-works-link').removeClass('active');
+    $(this).addClass('active');
+
+    let dataFilter = $(this).data('filter');
+
+    if (dataFilter === 'all') {
+      $('.j-works-item').show();
+      return;
+    }
+
+   
+    $('.j-works-item').each(function() {
+      let dataType = $(this).data('type');
+
+      if (dataFilter === dataType) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
+});
 
